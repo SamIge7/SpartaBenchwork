3f8513fedd9b5d0a9d48a549efec6e68fbb50a5c
SamIge7 <sige@spartaglobal.com>
2020-04-02 10:11:54 +0100

    Commit #2 2/4/20: Add drop down menu on Pies

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -21,14 +21,14 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
 
-            piesListViewModel.CurrentCategory = "Cheesecakes";
-            return View(piesListViewModel);
-        }
+        //    piesListViewModel.CurrentCategory = "Cheesecakes";
+        //    return View(piesListViewModel);
+        //}
 
         public IActionResult Details(int id)
         {
@@ -36,6 +36,30 @@ namespace BethanysPieShop.Controllers
             if (pie == null)
                 return NotFound();
             return View(pie);
+        }
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if(string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All Pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
     }
 }
